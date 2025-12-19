5512cf73ac23f98fbf922e058747ea79f0516f64
Jhonathan324 <158005754+Jhonathan324@users.noreply.github.com>
2025-12-19 13:04:35 -0300

    Adição do Menu de Pause

--- scripts/Player3D.gd
+++ scripts/Player3D.gd
@@ -3,7 +3,9 @@ extends CharacterBody3D
 @export var speed = 5.0
 @export var jump_velocity = 8
 @export var mouse_sensitivity = 0.002
-@export var deceleration_speed = 10.0  # Nova variável para desaceleração
+@export var deceleration_speed = 10.0 
+var paused = false
+var pause_menu_scene = preload("res://scenes/ui/PauseMenu.tscn")
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var camera = $PlayerCamera
@@ -17,6 +19,9 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
  
 func _input(event):
+	if event.is_action_just_pressed("pause"):
+		toggle_pause()
+	
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		camera.rotate_x(-event.relative.y * mouse_sensitivity)
@@ -68,3 +73,36 @@ func shoot():
 	# Tocar som de tiro
 	if has_node("ShootSound"):
 		$ShootSound.play()
+		
+	
+func toggle_pause():
+	paused = !paused
+	
+	if paused:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		get_tree().paused = true
+		
+		var pause_menu = pause_menu_scene.instantiate()
+		add_child(pause_menu)
+		
+		pause_menu.get_node("VBoxContainer/ContinueButton").pressed.connect(unpause)
+		pause_menu.get_node("VBoxContainer/MainMenuButton").pressed.connect(go_to_main_menu)
+		pause_menu.get_node("VBoxContainer/QuitButton").pressed.connect(quit_game)
+	else:
+		unpause()
+
+func unpause():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	get_tree().paused = false
+	
+	if has_node("PauseMenu"):
+		$PauseMenu.queue_free()
+	
+	paused = false
+
+func go_to_main_menu():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/ui/MainMenu.tscn")
+
+func quit_game():
+	get_tree().quit()
